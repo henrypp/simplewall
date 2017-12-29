48a9bb6951cb030a074e3328070ef1ab30457f46
henrypp <support@henrypp.org>
2017-12-29 20:27:40 +0600

    fixes

---
+++ build_resources.bat
@@ -0,0 +1,8 @@
+@echo off
+
+php "..\builder\make_resource.php" ".\src\resource.hpp"
+php "..\builder\make_locale.php" "simplewall" "simplewall" ".\bin\i18n" ".\src\resource.hpp" ".\bin\simplewall.lng"
+copy /y ".\bin\simplewall.lng" ".\bin\32\simplewall.lng"
+copy /y ".\bin\simplewall.lng" ".\bin\64\simplewall.lng"
+
+pause
