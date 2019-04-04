b3cc4c328d4a111b47e303431f02fdef186dcc03
henrypp <support@henrypp.org>
2019-04-05 02:26:04 +0600

    ++version

--- src/app.hpp
+++ src/app.hpp
@@ -1,13 +1,10 @@
 // simplewall
 // Copyright (c) 2016-2019 Henry++
 
-#ifndef __APP_H__
-#define __APP_H__
+#pragma once
 
 #define APP_NAME L"simplewall"
 #define APP_NAME_SHORT L"simplewall"
-#define APP_VERSION L"2.4.6"
-#define APP_VERSION_RES 2,4,6,0
+#define APP_VERSION L"2.5"
+#define APP_VERSION_RES 2,5,0,0
 #define APP_COPYRIGHT L"(c) 2016-2019 " _APP_AUTHOR L". All Rights Reversed."
-
-#endif // __APP_H__
