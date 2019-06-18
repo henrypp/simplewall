fb57e8d85d909264fa86b76f9f39d7868c0021dd
henrypp <support@henrypp.org>
2019-06-18 08:07:29 +0600

    upd definitions

--- src/helper.hpp
+++ src/helper.hpp
@@ -3,10 +3,19 @@
 
 #pragma once
 
-#define FMTADDR_AS_ARPA 0x01
-#define FMTADDR_AS_RULE 0x02
-#define FMTADDR_USE_PROTOCOL 0x04
-#define FMTADDR_RESOLVE_HOST 0x08
+#define FMTADDR_AS_ARPA 0x000001
+#define FMTADDR_AS_RULE 0x000002
+#define FMTADDR_USE_PROTOCOL 0x000004
+#define FMTADDR_RESOLVE_HOST 0x000008
+
+void _app_dereferenceapp (PVOID pdata);
+void _app_dereferenceappshelper (PVOID pdata);
+void _app_dereferencecolor (PVOID pdata);
+void _app_dereferencelog (PVOID pdata);
+void _app_dereferencenetwork (PVOID pdata);
+void _app_dereferencerule (PVOID pdata);
+void _app_dereferenceruleconfig (PVOID pdata);
+void _app_dereferencestring (PVOID pdata);
 
 UINT _app_gettab_id (HWND hwnd, size_t page_id = LAST_VALUE);
 void _app_settab_id (HWND hwnd, size_t page_id);
@@ -19,27 +28,28 @@ void _app_applycasestyle (LPWSTR buffer, size_t length);
 
 bool _app_formataddress (ADDRESS_FAMILY af, UINT8 proto, const PVOID ptr_addr, UINT16 port, LPWSTR* ptr_dest, DWORD flags);
 
-void _app_freearray (std::vector<PITEM_ADD>* ptr);
-void _app_freecache (MCACHE_MAP* ptr_map);
-void _app_freethreadpool (MTHREADPOOL* ptr_pool);
+void _app_freeobjects_map (OBJECTS_MAP& ptr_map, OBJECT_CLEANUP_CALLBACK cleanup_callback, bool is_forced);
+void _app_freeobjects_vec (OBJECTS_VEC& ptr_vec, OBJECT_CLEANUP_CALLBACK cleanup_callback);
+void _app_freethreadpool (THREADS_VEC* ptr_pool);
 void _app_freelogstack ();
-void _app_getappicon (ITEM_APP const* ptr_app, bool is_small, size_t* picon_id, HICON* picon);
-void _app_getdisplayname (size_t hash, ITEM_APP const* ptr_app, LPWSTR* extracted_name);
+
+void _app_getappicon (ITEM_APP* ptr_app, bool is_small, size_t* picon_id, HICON* picon);
+void _app_getdisplayname (size_t app_hash, ITEM_APP* ptr_app, LPWSTR* extracted_name);
 bool _app_getfileicon (LPCWSTR path, bool is_small, size_t* picon_id, HICON* picon);
 rstring _app_getshortcutpath (HWND hwnd, LPCWSTR path);
-bool _app_getsignatureinfo (size_t hash, LPCWSTR path, LPWSTR* psigner);
-bool _app_getversioninfo (size_t hash, LPCWSTR path, LPWSTR* pinfo);
-size_t _app_getposition (HWND hwnd, UINT listview_id, size_t idx);
+PR_OBJECT _app_getsignatureinfo (size_t app_hash, PITEM_APP ptr_app);
+PR_OBJECT _app_getversioninfo (size_t app_hash, PITEM_APP ptr_app);
+rstring _app_getportname (UINT16 port);
 rstring _app_getprotoname (UINT8 proto);
 rstring _app_getstatename (DWORD state);
 
-void _app_generate_connections (std::vector<PITEM_NETWORK>& ptr_arr);
+void _app_generate_connections (OBJECTS_MAP& ptr_map, CHECKER_MAP& checker_map);
 void _app_generate_packages ();
 void _app_generate_services ();
 
 void _app_generate_rulesmenu (HMENU hsubmenu, size_t app_hash);
 
-bool _app_item_get (EnumDataType type, size_t hash, rstring* display_name, rstring* real_path, PBYTE* lpdata, rstring* /*description*/);
+bool _app_item_get (EnumDataType type, size_t app_hash, rstring* display_name, rstring* real_path, PBYTE* lpdata);
 
 INT CALLBACK _app_listviewcompare_callback (LPARAM lp1, LPARAM lp2, LPARAM lparam);
 void _app_listviewsort (HWND hwnd, UINT listview_id, INT subitem = -1, bool is_notifycode = false);
@@ -57,10 +67,11 @@ void _app_resolvefilename (rstring& path);
 
 UINT _app_getlistview_id (EnumDataType type);
 
+size_t _app_getposition (HWND hwnd, UINT listview_id, size_t idx);
 void _app_showitem (HWND hwnd, UINT listview_id, size_t item, INT scroll_pos = -1);
 
 HBITMAP _app_bitmapfromico (HICON hicon, INT icon_size);
 HBITMAP _app_bitmapfrompng (HINSTANCE hinst, LPCWSTR name, INT icon_size);
 
-void _app_load_appxmanifest (PITEM_ADD ptr_item);
+void _app_load_appxmanifest (PITEM_APP_HELPER ptr_app_item);
 LPVOID _app_loadresource (HINSTANCE hinst, LPCWSTR res, LPCWSTR type, PDWORD size);
--- src/notifications.hpp
+++ src/notifications.hpp
@@ -4,26 +4,25 @@
 #pragma once
 
 void _app_notifycreatewindow ();
-bool _app_notifycommand (HWND hwnd, UINT ctrl_id, size_t timer_idx);
+bool _app_notifycommand (HWND hwnd, UINT button_id, size_t timer_idx);
 
-bool _app_notifyadd (HWND hwnd, PITEM_LOG const ptr_log, PITEM_APP const ptr_app);
-void _app_freenotify (size_t idx_orhash, bool is_idx, bool is_refresh);
-size_t _app_notifygetcurrent (HWND hwnd);
+bool _app_notifyadd (HWND hwnd, PR_OBJECT ptr_log_object, PITEM_APP ptr_app);
+void _app_freenotify (PITEM_APP ptr_app, bool is_refresh);
+void _app_freenotify (size_t app_hash, bool is_refresh);
 
-bool _app_notifyshow (HWND hwnd, size_t idx, bool is_forced, bool is_safety);
+size_t _app_notifyget_id (HWND hwnd);
+PR_OBJECT _app_notifyget_obj (size_t app_hash);
+
+bool _app_notifyshow (HWND hwnd, PR_OBJECT ptr_log_object, bool is_forced, bool is_safety);
+void _app_notifyshow (HWND hwnd, bool is_forced, bool is_safety);
 void _app_notifyhide (HWND hwnd);
 
 void _app_notifyplaysound ();
 
 bool _app_notifyrefresh (HWND hwnd, bool is_safety);
 
-void _app_notifysetpos (HWND hwnd);
-bool _app_notifysettimeout (HWND hwnd, UINT_PTR timer_id, bool is_create, UINT timeout);
-
-void _app_notifysetnote (HWND hwnd, UINT ctrl_id, rstring str);
 void _app_notifysettext (HDC hdc, HWND hwnd, UINT ctrl_id1, LPCWSTR text1, UINT ctrl_id2, LPCWSTR text2);
 
 HFONT _app_notifyinitfont (PLOGFONT plf, LONG height, LONG weight, LPCWSTR name, BOOL is_underline);
 
-void DrawFrameBorder (HDC hdc, HWND hwnd, COLORREF clr);
 LRESULT CALLBACK NotificationProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
