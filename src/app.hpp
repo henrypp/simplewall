11abf6ab071a8bd61f6588d2e32adf53714c89ea
henrypp <support@henrypp.org>
2019-06-18 08:06:14 +0600

    ++version

--- src/app.hpp
+++ src/app.hpp
@@ -5,6 +5,6 @@
 
 #define APP_NAME L"simplewall"
 #define APP_NAME_SHORT L"simplewall"
-#define APP_VERSION L"3.0.1"
-#define APP_VERSION_RES 3,0,1,0
+#define APP_VERSION L"3.0.2"
+#define APP_VERSION_RES 3,0,2,0
 #define APP_COPYRIGHT L"(c) 2016-2019 " _APP_AUTHOR L". All Rights Reversed."
