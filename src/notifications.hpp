4e7ea529169bd8abc9bc440a657b1707deed0f9e
henrypp <support@henrypp.org>
2019-06-21 20:40:35 +0600

    cosmetics

--- src/notifications.hpp
+++ src/notifications.hpp
@@ -10,11 +10,10 @@ bool _app_notifyadd (HWND hwnd, PR_OBJECT ptr_log_object, PITEM_APP ptr_app);
 void _app_freenotify (PITEM_APP ptr_app, bool is_refresh);
 void _app_freenotify (size_t app_hash, bool is_refresh);
 
-size_t _app_notifyget_id (HWND hwnd);
+size_t _app_notifyget_id (HWND hwnd, size_t current_id);
 PR_OBJECT _app_notifyget_obj (size_t app_hash);
 
 bool _app_notifyshow (HWND hwnd, PR_OBJECT ptr_log_object, bool is_forced, bool is_safety);
-void _app_notifyshow (HWND hwnd, bool is_forced, bool is_safety);
 void _app_notifyhide (HWND hwnd);
 
 void _app_notifyplaysound ();
