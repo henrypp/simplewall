38d1ccda0e995bb535aa74fc081b885086b689c3
henrypp <support@henrypp.org>
2020-04-30 19:34:35 +0600

    upd function declaration

--- src/helper.hpp
+++ src/helper.hpp
@@ -25,7 +25,7 @@ void _app_freethreadpool (THREADS_VEC* ptr_pool);
 void _app_freelogstack ();
 
 void _app_getappicon (const PITEM_APP ptr_app, bool is_small, PINT picon_id, HICON* picon);
-void _app_getdisplayname (size_t app_hash, ITEM_APP* ptr_app, LPWSTR* extracted_name);
+void _app_getdisplayname (size_t app_hash, PITEM_APP ptr_app, LPWSTR* extracted_name);
 bool _app_getfileicon (LPCWSTR path, bool is_small, PINT picon_id, HICON* picon);
 PR_OBJECT _app_getsignatureinfo (size_t app_hash, const PITEM_APP ptr_app);
 PR_OBJECT _app_getversioninfo (size_t app_hash, const PITEM_APP ptr_app);
