e7ce710a9a9953694199433646473004ecc90127
henrypp <support@henrypp.org>
2021-09-26 15:36:20 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"simplewall"
 #define APP_NAME_SHORT L"simplewall"
-#define APP_VERSION L"3.4.3"
-#define APP_VERSION_RES 3,4,3,0
+#define APP_VERSION L"3.5"
+#define APP_VERSION_RES 3,5,0,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2016-2021 " APP_AUTHOR L". All Rights Reversed."
