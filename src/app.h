da9e0b973b816d40ea94996b65f79db5122690cd
henrypp <support@henrypp.org>
2022-01-17 10:20:56 +0600

    ++year

--- src/app.h
+++ src/app.h
@@ -1,5 +1,5 @@
 // simplewall
-// Copyright (c) 2016-2021 Henry++
+// Copyright (c) 2016-2022 Henry++
 
 #pragma once
 
@@ -8,4 +8,4 @@
 #define APP_VERSION L"3.6.2"
 #define APP_VERSION_RES 3,6,2,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2016-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2016-2022 " APP_AUTHOR L". All Rights Reversed."
