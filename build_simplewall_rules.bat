e5085b26551e4fc256f6fd2618aba4287dad16e2
henrypp <support@henrypp.org>
2023-10-18 21:25:20 +0600

    upd build_simplewall_rules script

--- build_simplewall_rules.bat
+++ build_simplewall_rules.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\builder
-call build_simplewall_rules update %~dp0bin\profile_internal.sp %~dp0bin\profile_internal.sp
+call build_simplewall_rules update %~dp0bin\profile_internal.xml %~dp0bin\profile_internal.xml
 
 pause
