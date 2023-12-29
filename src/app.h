ea03da6da60f4e5231d129b9af51d432d626e312
henrypp <sforce5@mail.ru>
2023-12-30 03:48:57 +0600

    ++year

--- src/app.h
+++ src/app.h
@@ -1,5 +1,5 @@
 // simplewall
-// Copyright (c) 2016-2023 Henry++
+// Copyright (c) 2016-2024 Henry++
 
 #pragma once
 
@@ -8,4 +8,4 @@
 #define APP_VERSION L"3.7.6"
 #define APP_VERSION_RES 3,7,6,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2016-2023 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2016-2024 " APP_AUTHOR L". All Rights Reversed."
