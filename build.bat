c1b3440719827b4ef970f75cd64b002d84d96193
henrypp <sforce5@mail.ru>
2025-02-20 16:05:59 +0500

    ++version

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\builder
-call build simplewall 3.8.5 simplewall
+call build simplewall 3.8.6 simplewall
 
 pause
--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"simplewall"
 #define APP_NAME_SHORT L"simplewall"
-#define APP_VERSION L"3.8.5"
-#define APP_VERSION_RES 3,8,5,0
+#define APP_VERSION L"3.8.6"
+#define APP_VERSION_RES 3,8,6,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2016-2025 " APP_AUTHOR L". All Rights Reversed."
