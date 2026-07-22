d112f45714c819588c3604117291f2a5b10ae516
henrypp <sforce5@mail.ru>
2026-07-22 22:25:08 +0500

    fix version

--- src/app.h
+++ src/app.h
@@ -5,8 +5,8 @@
 
 #define APP_NAME L"simplewall"
 #define APP_NAME_SHORT L"simplewall"
-#define APP_VERSION L"3.9"
-#define APP_VERSION_RES 3,9,0,0
+#define APP_VERSION L"3.9.1"
+#define APP_VERSION_RES 3,9,1,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2016-2026 " APP_AUTHOR L". All Rights Reversed."
 
